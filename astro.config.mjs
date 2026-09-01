d0dbcfdc6bbba7c047d1f01c8dddfc2d9db90bd1
Benjamin R. Bray <benrbray@gmail.com>
2026-09-01 12:51:59 +0900

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://benrbray.com/courses',
+  site: 'https://benrbray.com',
+  base: '/courses',
   build: {
     format: "file"
   },
